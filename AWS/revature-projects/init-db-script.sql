1540cdb2f34173ac5eb3d7753000a84beca565dc
David R. Saeva <drsaeva@gmail.com>
2018-03-12 21:55:43 -0400

    Update init-db-script.sql

--- AWS/revature-projects/init-db-script.sql
+++ AWS/revature-projects/init-db-script.sql
@@ -31,9 +31,9 @@ END;
 --Users table 
 CREATE TABLE users (
     id              NUMBER(5)     PRIMARY KEY,
-    username        VARCHAR2(8)   UNIQUE    NOT NULL,
+    username        VARCHAR2(12)   UNIQUE    NOT NULL,
     userpw          VARCHAR2(16)  CONSTRAINT pw_gt_7 CHECK (LENGTH(userpw) > 7),   
-    fullname        VARCHAR2(16)  NOT NULL,
+    fullname        VARCHAR2(25)  NOT NULL,
     access_level    NUMBER(1)     CONSTRAINT alvl_limits CHECK (access_level < 2 OR access_level > -2)
 );
 /
@@ -67,7 +67,7 @@ CREATE TABLE customers_accounts (
 --Applications table
 CREATE TABLE applications (
     id              NUMBER(5)   PRIMARY KEY,
-    username        VARCHAR2(8) UNIQUE  NOT NULL,
+    username        VARCHAR2(12) UNIQUE  NOT NULL,
     status          NUMBER(1)   CONSTRAINT  app_status_limit CHECK (status < 2 OR status > -2),
     joint           NUMBER(1)   CONSTRAINT  joint_boolean CHECK (joint > -1 OR joint < 2),
     joint_acc_num   NUMBER(10), CONSTRAINT jt_ac_num_fk FOREIGN KEY (joint_acc_num) REFERENCES accounts(num),
